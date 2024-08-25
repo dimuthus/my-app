183bcf7facc62f72ce220425bbf1eb4749795f97
dimuthus <dimuthusuranga@gmail.com>
2024-08-25 17:20:06 +0200

    4-cleaning up project and File Structure

---
+++ app/(pages)/(website)/(home)/page.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Image from 'next/image'
+
+const Home = () => {
+  return (
+    <>
+    <div>home after </div>
+    <Image src="/assets/images/logo.svg" width="200" height="200" alt='logo'/>
+    </>
+    
+  )
+}
+
+export default Home
